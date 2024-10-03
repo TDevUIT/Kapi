c6278887eb0669d93cdfa3011fd134161f2dc93d
vanthaita <thaitv225@gmail.com>
2024-10-03 15:01:47 +0700

    feat(server): get vocabulary statuses/notifications by user id

--- apps/server/src/module/user/user.controller.ts
+++ apps/server/src/module/user/user.controller.ts
@@ -1,30 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ResponseMessage } from 'src/decorator/response-message.decorator';
-
+import { Request as ExpressRequest } from 'express';
+import { AuthGuard as JWTGuard } from '../auth/google.guard';
+import { Notification, VocabularyStatus } from '@prisma/client';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
-  @ResponseMessage('Get user')
-  getUser() {
-    return [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-      },
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-      },
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'john.doe@example.com',
-      },
-    ];
+  @UseGuards(JWTGuard)
+  @Get('notifications')
+  @ResponseMessage('Get user notifications')
+  async getNotifications(@Req() req: ExpressRequest): Promise<Notification[]> {
+    const userId = req['user'].id;
+    if (!userId) {
+      throw new Error('User ID not found');
+    }
+    return this.userService.getNotificationsByUserId(userId);
+  }
+
+  @UseGuards(JWTGuard)
+  @Get('vocabulary-statuses')
+  @ResponseMessage('Get user vocabulary statuses')
+  async getVocabularyStatuses(
+    @Req() req: ExpressRequest,
+  ): Promise<VocabularyStatus[]> {
+    const userId = req['user'].id;
+    if (!userId) {
+      throw new Error('User ID not found');
+    }
+    return this.userService.getVocabularyStatusesByUserId(userId);
   }
 }
