82f18aea600405c63dbf46bdbe1a7803828121a7
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2024-10-04 08:48:05 +0700

    Update lesson page

--- apps/native/app/(lesson)/_layout.tsx
+++ apps/native/app/(lesson)/_layout.tsx
@@ -1,12 +1,7 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar, Tab } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-
 const LessonLayout = () => {
-  const Tab = createBottomTabNavigator();
   return (
     <>
       <Stack>
