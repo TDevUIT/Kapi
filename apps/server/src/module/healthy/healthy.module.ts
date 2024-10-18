4706df85e533d43f19f1a35ce60fb975ece9e306
LeeinUITk17 <22521276@gm.uit.edu.vn>
2024-10-18 13:54:25 +0700

    fix: add missing import module prisma healthy notification

--- apps/server/src/module/healthy/healthy.module.ts
+++ apps/server/src/module/healthy/healthy.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthController, HealthService } from './healthy.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [HealthController],
   providers: [HealthService],
 })
--- apps/server/src/module/notification/notification.module.ts
+++ apps/server/src/module/notification/notification.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NotificationsController } from './notification.controller';
 import { NotificationsService } from './notification.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
